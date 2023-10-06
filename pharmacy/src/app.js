8b8ab8d19089f672e8a12e70ef396b6b03bb0c08
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-10-06 08:53:18 +0300

    adding the microservices structure and files

---
+++ pharmacy/src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+// import the pharmacyapi 
+
+dotenv.config();
+const app = express();
+
+const mongoURL = process.env.MONGO_URI;
+
+const connect = async () => {
+    try {
+        await mongoose.connect(mongoURL);
+        console.log("Database connected");
+    } catch (err) {
+        console.error("Error connecting to the database:", err); 
+        process.exit(1); 
+    }
+};
+
+await connect();
+
+app.use(express.json());
+
+// call the api class here and pass the app as parm
+
+const port = process.env.PORT || 8003;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ pharmacy/src/database/models/Pharmacist.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import UserSchema from "./UserSchema.js";
+
+const Pharmacist = mongoose.Schema({
+    userData: {
+        type: UserSchema,
+        required: true
+    },
+    hourlyRate: {
+        type: Number,
+        required: true
+    },
+    affiliation: {
+        type: String,
+        required: true
+    },
+    educationalBackground: {
+        type: String,
+        required: true
+    },
+    //.....
+});
+
+const PharmacistModel = mongoose.model('Pharmacist', Pharmacist);
+
+export default PharmacistModel;
