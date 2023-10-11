fe734b3830733628d969ca6a54a89b6f656735ce
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-10-11 22:54:00 +0300

    feat : adding the frontend view of the pharmacist's information

--- pharmacy/src/api/PharmacistAPI.js
+++ pharmacy/src/api/PharmacistAPI.js
@@ -6,12 +6,12 @@ import { ERROR_STATUS_CODE, NOT_FOUND_STATUS_CODE, OK_STATUS_CODE } from '../uti
 export const pharmacist = (app) => {
 	const service = new PharmacistService();
 
-	app.get('/get-all-patients', async (req,res) => {
-		const getPatientsURL = 'http://localhost:8002/all-patients';
+	app.get('/patients', async (req,res) => {
+		const getPatientsURL = `${PATIENTS_BASE_URL}/patients`;
 		try{
 			const response = await axios.get(getPatientsURL);
 			const allPatients = response.data;
-			res.status(OK_STATUS_CODE).json(allPatients);
+			res.status(OK_STATUS_CODE).json({allPatients});
 		}catch(err){
 			res.status(ERROR_STATUS_CODE).json({ err : err.message });
 		}
@@ -19,23 +19,7 @@ export const pharmacist = (app) => {
        
 	});
 
-	app.get('/pharmacist', async(req,res) => {
-		const id = req.body.id;
-		//const email = req.body.email;
-		try{
-			const pharmacist = await service.getPharmacist(id);
-			if(pharmacist){
-				res.status(OK_STATUS_CODE).json({ pharmacist });
-			}else{
-				res.status(NOT_FOUND_STATUS_CODE).json({ message:'pharmacist not found' });
-			}
-		}catch(err){
-			res.status(ERROR_STATUS_CODE).json({ err : err.message });
-		}
-        
-	});
-
-	app.get('/pharmacist', async(req,res) => {
+	app.get('/pharmacists', async(req,res) => {
 		try{
 			const pharmacists = await service.getAllPharmacists();
 			if(pharmacists){
--- pharmacy/src/utils/Constants.js
+++ pharmacy/src/utils/Constants.js
@@ -2,5 +2,7 @@ export const PORT = 8003;
 export const OK_STATUS_CODE = 200;
 export const NOT_FOUND_STATUS_CODE = 404;
 export const ERROR_STATUS_CODE = 500;
+export const PATIENTS_BASE_URL = 'http://localhost:8002';
+
 
 export const MEDICINE_FOLDER_NAME = 'medicines';
