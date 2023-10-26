e39ec5114bf4f2fc10a3f9d34a3472e60e9296cd
amir-awad <amirawad004@gmail.com>
2023-10-26 08:51:01 +0300

    added initial ui for the cart page

--- client/src/pages/cart/Cart.js
+++ client/src/pages/cart/Cart.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import { Avatar, Paper, Typography } from '@mui/material';
 import { useUserContext } from 'hooks/useUserContext';
 import { pharmacyAxios } from '../../utils/AxiosConfig';
+import { Box, color } from '@mui/system';
+import Button from '@mui/material/Button';
+import { PHARMACY_BASE_URL } from 'utils/Constants';
 
 const Cart = () => {
 	const user = useUserContext();
@@ -20,33 +24,70 @@ const Cart = () => {
 		console.log('cart items: ', cartItems);
 	}, []);
 
-	const handleDeleteMedicine = (id) => {
-		pharmacyAxios
-			.delete(`/cart/medicines/${id}`)
-			.then((response) => {
-				console.log(response.data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+	// const handleDeleteMedicine = (id) => {
+	// 	pharmacyAxios
+	// 		.delete(`/cart/medicines/${id}`)
+	// 		.then((response) => {
+	// 			console.log(response.data);
+	// 		})
+	// 		.catch((error) => {
+	// 			console.log(error);
+	// 		});
+	// };
 
 	return (
-		<div>
-			<h1>Cart</h1>
-			{Array.isArray(cartItems) &&
-				cartItems.map((item) => (
-					<div key={item.medicine._id}>
-						<h3>{item.medicine.name}</h3>
-						<p>{item.medicine.description}</p>
-						<p>{item.medicine.price}</p>
-						<p>{item.quantity}</p>
-						<button onClick={() => handleDeleteMedicine(item.medicine._id)}>
-							Delete
-						</button>
-					</div>
-				))}
-		</div>
+		<>
+			<Box
+				sx={{ p: 4 }}
+				display='flex'
+				justifyContent='center'
+				flexDirection='column'
+				alignItems='left'
+			>
+				<Typography variant='h3' color={color.black}>
+					Your Cart
+				</Typography>
+				<Paper
+					elevation={0}
+					sx={{
+						p: 30,
+						pl: 10,
+						display: 'flex',
+						flexDirection: 'column',
+						height: 240,
+						justifyContent: 'center',
+					}}
+				>
+					{Array.isArray &&
+						cartItems.map((item) => (
+							<Box key={item.medicine.id}>
+								<Box
+									display='flex'
+									sx={{ pt: 3, pb: 2 }}
+									alignItems='start'
+									justifyContent='space-between'
+								></Box>
+								<Avatar
+									src={`${PHARMACY_BASE_URL}/medicines/${item.medicine._id}/pictures`}
+									sx={{ width: 80, height: 80, mr: 2 }}
+								/>
+								<Box display='flex' flexDirection={'column'}>
+									<Typography variant='h6'>{item.medicine.name}</Typography>
+									<Typography variant='body2'>
+										{item.medicine.description}
+									</Typography>
+								</Box>
+								<Typography variant='body1' justifyContent={'end'}>
+									{item.medicine.price}
+								</Typography>
+							</Box>
+						))}
+				</Paper>
+				<Button variant='contained' color='primary'>
+					Proceed to Checkout
+				</Button>
+			</Box>
+		</>
 	);
 };
 
