837a98acbe60ebe08c251fc91dc847d9b6c65a51
Mohamed Khaled <113786472+Mohamed-Khaled308@users.noreply.github.com>
2023-11-09 20:58:21 +0200

    feat: added orders, addresses, checkout pages

--- client/src/utils/Constants.js
+++ client/src/utils/Constants.js
@@ -1,6 +1,36 @@
-export const CLINIC_BASE_URL  = 'http://localhost:8001';
+export const CLINIC_BASE_URL = 'http://localhost:8001';
 export const PATIENT_BASE_URL = 'http://localhost:8002';
 export const PHARMACY_BASE_URL = 'http://localhost:8003';
 
 export const PATIENT_TYPE_ENUM = 'patient';
 export const ADMIN_TYPE_ENUM = 'admin';
+
+const APPOINTMENT_STATUS = ['COMPLETE', 'UNCOMPLETE', 'CANCELED'];
+export const DATE_FILTER_ARRAY = ['Last week', 'Last month', 'Last year'];
+
+export const APPOINTMENT_FILTER_ARRAY = [
+    {
+        attribute: 'Appointment Status',
+        values: APPOINTMENT_STATUS,
+    },
+    {
+        attribute: 'Date',
+        values: DATE_FILTER_ARRAY,
+    },
+];
+
+export const DOCTOR_TYPE_ENUM = 'doctor';
+
+export const ADDRESS_ATTRIBUTES = [
+    'city',
+    'street',
+    'buildingName',
+    'phoneNumber',
+];
+
+export const PENDING_STATUS = 'pending';
+export const CANCELLED_STATUS = 'cancelled';
+
+export const DATE_FORAMT = 'MMMM Do YYYY, h:mm a';
+
+export const ZERO_INDEX = 0;
