4e73db4071859b3ce761aab7a065b6a007a66f62
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2023-11-10 19:39:05 +0200

    remove unused import

--- pharmacy/src/tests/api-tests/MedicineAPI.test.js
+++ pharmacy/src/tests/api-tests/MedicineAPI.test.js
@@ -1,118 +1,117 @@
 import request from 'supertest';
 import app from '../../../app.js';
 import {
-    connectDBTest,
-    disconnectDBTest
+	connectDBTest,
+	disconnectDBTest
 } from '../../utils/TestingUtils.js';
 import {
-    OK_STATUS_CODE,
-    ERROR_STATUS_CODE,
-    NOT_FOUND_STATUS_CODE,
+	OK_STATUS_CODE,
+	ERROR_STATUS_CODE,
+	NOT_FOUND_STATUS_CODE,
 } from '../../utils/Constants.js';
 import MedicineModel from '../../database/models/Medicine.js';
 import generateMedicine from '../model-generators/generateMedicine.js';
 import { describe, beforeEach, afterEach, expect, it, jest } from '@jest/globals';
 import { faker } from '@faker-js/faker';
-import axios from 'axios';
 
 jest.mock('axios');
 
 describe('GET /medicines', () => {
 
-    const fetchMedicines = async () => { return await request(app).get(`/medicines`) }
+	const fetchMedicines = async () => { return await request(app).get('/medicines'); };
 
-    beforeEach(async () => {
-        await connectDBTest();
-    });
+	beforeEach(async () => {
+		await connectDBTest();
+	});
 
-    it('should return 200 OK and retrieve the medicines correctly', async () => {
+	it('should return 200 OK and retrieve the medicines correctly', async () => {
 
-        for (let i = 0; i < 5; i++) {
-            const medicine = new MedicineModel(generateMedicine());
-            await medicine.save();
+		for (let i = 0; i < 5; i++) {
+			const medicine = new MedicineModel(generateMedicine());
+			await medicine.save();
 
-        }
+		}
 
-        const res = await fetchMedicines();
+		const res = await fetchMedicines();
 
-        expect(res.status).toBe(OK_STATUS_CODE);
-        expect(res._body.medicines.length).toBe(5);
-    });
+		expect(res.status).toBe(OK_STATUS_CODE);
+		expect(res._body.medicines.length).toBe(5);
+	});
 
-    afterEach(async () => {
+	afterEach(async () => {
 
-        await disconnectDBTest();
-    });
+		await disconnectDBTest();
+	});
 });
 
 describe('PATCH /medicines/:id', () => {
 
-    const updateMedicines = async (id, updatedMedicine) => { return await request(app).patch(`/medicines/${id}`).send(updatedMedicine); }
+	const updateMedicines = async (id, updatedMedicine) => { return await request(app).patch(`/medicines/${id}`).send(updatedMedicine); };
 
-    beforeEach(async () => {
-        await connectDBTest();
-    });
+	beforeEach(async () => {
+		await connectDBTest();
+	});
 
-    it('should return 200 OK and update the medicine correctly', async () => {
-        const medicine = new MedicineModel(generateMedicine());
-        await medicine.save();
-        const id = medicine._id;
-        const updatedMedicine = {
-            name: faker.commerce.productName(),
-            price: medicine.price,
-            description: medicine.description,
-            pictureName: medicine.pictureName,
-            quantity: medicine.quantity,
-            sales: medicine.sales,
-            medicinalUse: medicine.medicinalUse,
-            activeIngerdients: medicine.activeIngerdients,
-        };
-        const res = await updateMedicines(id, updatedMedicine);
-        expect(res.status).toBe(OK_STATUS_CODE);
-    });
+	it('should return 200 OK and update the medicine correctly', async () => {
+		const medicine = new MedicineModel(generateMedicine());
+		await medicine.save();
+		const id = medicine._id;
+		const updatedMedicine = {
+			name: faker.commerce.productName(),
+			price: medicine.price,
+			description: medicine.description,
+			pictureName: medicine.pictureName,
+			quantity: medicine.quantity,
+			sales: medicine.sales,
+			medicinalUse: medicine.medicinalUse,
+			activeIngerdients: medicine.activeIngerdients,
+		};
+		const res = await updateMedicines(id, updatedMedicine);
+		expect(res.status).toBe(OK_STATUS_CODE);
+	});
 
 
-    it('should return 404 ERROR with the unknown id', async () => {
-        const id = faker.database.mongodbObjectId();
-        const updatedMedicine = new MedicineModel(generateMedicine());
-        const res = await updateMedicines(id, updatedMedicine);
-        expect(res.status).toBe(NOT_FOUND_STATUS_CODE);
-    });
+	it('should return 404 ERROR with the unknown id', async () => {
+		const id = faker.database.mongodbObjectId();
+		const updatedMedicine = new MedicineModel(generateMedicine());
+		const res = await updateMedicines(id, updatedMedicine);
+		expect(res.status).toBe(NOT_FOUND_STATUS_CODE);
+	});
 
-    afterEach(async () => {
+	afterEach(async () => {
 
-        await disconnectDBTest();
-    });
+		await disconnectDBTest();
+	});
 });
 
 describe('POST /medicines', () => {
 
-    const addMedicine = async (newMedicine) => { return await request(app).post(`/medicines`).send(newMedicine); }
+	const addMedicine = async (newMedicine) => { return await request(app).post('/medicines').send(newMedicine); };
 
-    beforeEach(async () => {
-        await connectDBTest();
-    });
+	beforeEach(async () => {
+		await connectDBTest();
+	});
 
-    it('should return 200 OK and add the medicine correctly', async () => {
-        const medicine = generateMedicine();
-        const newMedicine = {
-            newMedicine: JSON.stringify(medicine)
-        }
-        const res = await addMedicine(newMedicine);
-        expect(res.status).toBe(OK_STATUS_CODE);
-        const medicines = await MedicineModel.find();
-        expect(medicines.length).toBe(1);
-    });
+	it('should return 200 OK and add the medicine correctly', async () => {
+		const medicine = generateMedicine();
+		const newMedicine = {
+			newMedicine: JSON.stringify(medicine)
+		};
+		const res = await addMedicine(newMedicine);
+		expect(res.status).toBe(OK_STATUS_CODE);
+		const medicines = await MedicineModel.find();
+		expect(medicines.length).toBe(1);
+	});
 
 
-    it('should return 500 ERROR', async () => {
-        const medicine = { name: 'medicine missing attributes' };
-        const res = await addMedicine(medicine);
-        expect(res.status).toBe(ERROR_STATUS_CODE);
-    });
+	it('should return 500 ERROR', async () => {
+		const medicine = { name: 'medicine missing attributes' };
+		const res = await addMedicine(medicine);
+		expect(res.status).toBe(ERROR_STATUS_CODE);
+	});
 
-    afterEach(async () => {
+	afterEach(async () => {
 
-        await disconnectDBTest();
-    });
+		await disconnectDBTest();
+	});
 });
--- pharmacy/src/tests/model-generators/generateMedicine.js
+++ pharmacy/src/tests/model-generators/generateMedicine.js
@@ -1,16 +1,16 @@
 import { faker } from '@faker-js/faker';
 
 const generateMedicine = () => {
-    return {
-        name: faker.commerce.productName(),
-        price: faker.commerce.price(),
-        description: faker.lorem.sentence(),
-        pictureName: faker.image.url,
-        quantity: faker.number.int(),
-        sales: faker.number.int(),
-        medicinalUse: faker.lorem.sentence(),
-        activeIngerdients: faker.lorem.words(),
-    };
+	return {
+		name: faker.commerce.productName(),
+		price: faker.commerce.price(),
+		description: faker.lorem.sentence(),
+		pictureName: faker.image.url,
+		quantity: faker.number.int(),
+		sales: faker.number.int(),
+		medicinalUse: faker.lorem.sentence(),
+		activeIngerdients: faker.lorem.words(),
+	};
 };
 
 export default generateMedicine;
