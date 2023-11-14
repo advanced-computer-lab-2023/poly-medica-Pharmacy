4b6813d31e900496c6ee87f0cfbc74458ce19852
Abdelrahman Elmeky <65960126+Aelmeky@users.noreply.github.com>
2023-11-14 21:36:42 +0200

    feat : enhancing the payment options frontend

--- client/src/pages/payment/PaymentOptions.js
+++ client/src/pages/payment/PaymentOptions.js
@@ -1,9 +1,13 @@
-import { Radio, FormControlLabel  , RadioGroup, FormLabel, FormControl } from '@mui/material';
+import { Radio, FormControlLabel, RadioGroup, FormLabel, FormControl, Container, ListItemAvatar, Avatar, Typography } from '@mui/material';
 import { patientAxios } from 'utils/AxiosConfig.js';
 import { useUserContext } from 'hooks/useUserContext';
 import Swal from 'sweetalert2';
 import { useState, useEffect } from 'react';
-import '../assets/css/swalStyle.css';
+import '../../assets/css/swalStyle.css';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import PaymentsIcon from '@mui/icons-material/Payments';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+
 
 const PaymentOptions = ({ handleChange, value }) => {
     const [amountInWallet, setAmountInWallet] = useState(0);
@@ -11,12 +15,12 @@ const PaymentOptions = ({ handleChange, value }) => {
     const userId = user.id;
     useEffect(
         () => {
-          patientAxios.get(`/patients/${userId}/wallet`).then((response) => {
-              setAmountInWallet(response.data.walletAmount);
-        }).
-          catch(error => {
-            Swal.fire('error', error, 'error');
-          });
+            patientAxios.get(`/patients/${userId}/wallet`).then((response) => {
+                setAmountInWallet(response.data.walletAmount);
+            }).
+                catch(error => {
+                    Swal.fire('error', error, 'error');
+                });
         }, []);
 
     return (
@@ -29,9 +33,48 @@ const PaymentOptions = ({ handleChange, value }) => {
                 onChange={handleChange}
                 sx={{ my: 1 }}
             >
-                <FormControlLabel  value='credit-card' control={<Radio />} label='Credit Card' />
-                <FormControlLabel  value='wallet' control={<Radio />} label={`Poly-Wallet $${amountInWallet}`} />
-                <FormControlLabel  value='cash' control={<Radio />} label='Cash on deleviry' />
+                <FormControlLabel
+                    value='credit-card'
+                    control={<Radio />}
+                    label={
+                        <Container style={{ display: 'flex', alignItems: 'center', margin: '8px 0' }}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <CreditCardIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <Typography variant="body1" style={{ fontSize: '16px', marginLeft: '2' }}>CREDIT CARD</Typography>
+                        </Container>
+                    }
+                />
+                <FormControlLabel
+                    value='wallet'
+                    control={<Radio />}
+                    label={(
+                        <Container style={{ display: 'flex', alignItems: 'center', margin: '8px 0' }}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <AccountBalanceWalletIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <Typography variant="body1" style={{ fontSize: '16px', marginLeft: '2' }}>{`POLY-WALLET $ ${amountInWallet}`}</Typography>
+                        </Container>
+                    )}
+                />
+                <FormControlLabel
+                    value='cash'
+                    control={<Radio />}
+                    label={(
+                        <Container style={{ display: 'flex', alignItems: 'center', margin: '8px 0' }}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <PaymentsIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <Typography variant="body1" style={{ fontSize: '16px', marginLeft: '2' }}>CASH ON DELEVIRY</Typography>
+                        </Container>
+                    )}
+                />
             </RadioGroup>
         </FormControl>
     );
