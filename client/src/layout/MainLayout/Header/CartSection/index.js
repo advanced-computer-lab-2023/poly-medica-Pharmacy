e500e8181ba241ed8f36f28d72f7d240f27c3e94
amir-awad <amirawad004@gmail.com>
2023-12-15 18:56:49 +0200

    changed cart theme colors

--- client/src/layout/MainLayout/Header/CartSection/index.js
+++ client/src/layout/MainLayout/Header/CartSection/index.js
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import { useCartContext } from '../../../../contexts/CartContext';
 import { useNavigate } from 'react-router-dom';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { IconButton, Badge } from '@mui/material';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Badge, Avatar, Box, ButtonBase } from '@mui/material';
 import { useUserContext } from 'hooks/useUserContext';
 import { pharmacyAxios } from 'utils/AxiosConfig';
+import { useTheme } from '@mui/material/styles';
 
 // ==============================|| CART ||============================== //
 
 const CartSection = () => {
+	const theme = useTheme();
 	const navigate = useNavigate();
 	const { cartLength, updateCartLength } = useCartContext();
 	const { user } = useUserContext();
@@ -27,11 +29,41 @@ const CartSection = () => {
 	}, []);
 
 	return (
-		<IconButton onClick={() => navigate('pages/cart')}>
-			<Badge badgeContent={cartLength} color='secondary'>
-				<ShoppingCartIcon />
-			</Badge>
-		</IconButton>
+		<Box
+			sx={{
+				ml: 2,
+				mr: 3,
+				[theme.breakpoints.down('md')]: {
+					mr: 2,
+				},
+			}}
+		>
+			<ButtonBase sx={{ borderRadius: '12px' }}>
+				<Badge badgeContent={cartLength} color='secondary'>
+					<Avatar
+						variant='rounded'
+						sx={{
+							...theme.typography.commonAvatar,
+							...theme.typography.mediumAvatar,
+							transition: 'all .2s ease-in-out',
+							'&[aria-controls="menu-list-grow"]': {
+								background: theme.palette.secondary.light,
+								color: theme.palette.secondary.dark,
+							},
+							'&:hover': {
+								background: theme.palette.secondary.dark,
+								color: theme.palette.secondary.light,
+							},
+						}}
+						aria-controls={open ? 'menu-list-grow' : undefined}
+						aria-haspopup='true'
+						onClick={() => navigate('pages/cart')}
+					>
+						<ShoppingCartOutlinedIcon />
+					</Avatar>
+				</Badge>
+			</ButtonBase>
+		</Box>
 	);
 };
 
