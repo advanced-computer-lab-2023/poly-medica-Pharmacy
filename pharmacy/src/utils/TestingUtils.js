9a03989010f3f04bf047c089286717db097b35b8
amir-awad <amirawad004@gmail.com>
2024-01-18 22:35:30 +0200

    closed kafka producer after tests finish running

--- pharmacy/src/utils/TestingUtils.js
+++ pharmacy/src/utils/TestingUtils.js
@@ -1,3 +1,4 @@
+const { producer } = require('../producers/MedicineProducer.js');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
@@ -14,6 +15,7 @@ const connectDBTest = async () => {
 
 const disconnectDBTest = async () => {
 	try {
+		producer.close();
 		await mongoose.connection.db.dropDatabase();
 		await mongoose.disconnect();
 	} catch (err) {
